23ae8ea452b6c7614baa3f6cc8385f519a54e747
rukunuddin29 <rukunuddin2003@gmail.com>
2024-09-04 19:24:03 +0530

    Fix: 3

--- src/Component/Body.jsx
+++ src/Component/Body.jsx
@@ -7,7 +7,7 @@ function Body() {
   useEffect(() => {
     const fetchArticles = async () => {
       try {
-        const url = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${import.meta.env.VITE_API_KEY}`;
+        const url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=09c537c5620d49f9934963bbae0e9ec6`;
         const response = await fetch(url);
         const data = await response.json();
         setArticles(data.articles || []); // Set articles or empty array if none found
